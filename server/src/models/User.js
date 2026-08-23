535e3dd4f871dddce627fff33d4f259b56603ea7
Shourya26-08 <aryanbhatnagar13@gmail.com>
2026-08-23 17:05:51 +0530

    feat: add user data model

---
+++ server/src/models/User.js
@@ -0,0 +1,1 @@
+import mongoose from 'mongoose';const schema=new mongoose.Schema({name:{type:String,required:true,trim:true},email:{type:String,required:true,unique:true,lowercase:true,trim:true},password:{type:String,required:true,minlength:6}},{timestamps:true});export default mongoose.model('User',schema);
